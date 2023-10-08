f0ddcbcb8e9c3dac7725a4ad1ccc1cd3a5c4609c
krishanujadiya <krishanujadiya@gmail.com>
2023-10-08 21:53:16 +0530

    Verify JWT token for LoggedIn User

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+
 require('dotenv').config()
 
 const mongoose = require('mongoose')
@@ -20,16 +24,17 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
 // // JWT token verification middleware
-// function verifyToken(req, res, next) {
-//   const token = req.header('Authorization');
-//   if (!token) return res.status(401).json({ error: 'Access denied' });
-
-//   jwt.verify(token, 'secret_key', (err, decoded) => {
-//     if (err) return res.status(401).json({ error: 'Invalid token' });
-//     req.userId = decoded._id;
-//     next();
-//   });
-// }
+function verifyToken(req, res, next) {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ error: 'Access denied' });
+  jwt.verify(token, 'secretKey', (err, decoded) => {
+    if (err) {
+    return res.status(401).json({ error: 'Invalid token' });
+    }
+    req.userId = decoded._id;
+    next();
+  });
+}
 
 // Protected routes
 app.use(logger('dev'));
@@ -40,7 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/', productRouter);
+app.use('/', verifyToken, productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
